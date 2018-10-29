0a1a0560d822ba3480ebac1762a9601ab7cf9ae7
Kenta HANDA <handa.kenta@isid.co.jp>
2018-10-29 20:31:20 +0900

    add user

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -44,6 +44,7 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
         Gate::define('viewNova', function ($user) {
             return in_array($user->email, [
                 //
+                'handa@inf-6c.info'
             ]);
         });
     }
